88d03d2e601137533e8570deebf241be86a9f252
Carlotaml21 <carlotaml21@gmail.com>
2025-01-28 15:34:30 +0100

    add person class

---
+++ src/main/java/first/class/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+    
+        Person person = new Person("Carlota", "Menéndez", "71890587A", 1990);
+        System.out.println(person);
+    }
+}
---
+++ src/main/java/first/class/java/Person.java
@@ -0,0 +1,27 @@
+package first.class.java;
+
+
+public class Person {
+    private String name;
+    private String surname;
+    private String idNumber;
+    private int birthYear;
+
+    public Person(){
+
+    }
+
+    public Person (String name, String surname, String idNumber, int birthYear) {
+        this.name = name;
+        this.surname = surname;
+        this.idNumber = idNumber;
+        this.birthYear = birthYear;
+    }
+
+    @Override 
+    public String toString(){
+        return "Person{" + "name='" + name + '\''+ ", surname='" + surname +
+        '\'' + ", idNumber='" + idNumber + '\'' + ", birthYear='" + birthYear + '}';
+    }
+}
+
